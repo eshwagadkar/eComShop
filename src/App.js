4adedb95db4771c83dfa3a96ddb93e44369c8264
eshwagadkar <eshwagadkar007@gmail.com>
2023-06-24 12:40:49 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+const App = () => {
+  
+  
+  return (
+    <div className="categories-container">
+        {/* Category 1  */}
+        <div className="category-container">
+            {/* <img /> */}
+          <div className="category-body-container">
+            <h2>Hats</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
