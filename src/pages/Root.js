3b05718a75a39ebcca8d5aef16fc5a56aa8e4d3f
eshwagadkar <eshwagadkar007@gmail.com>
2023-07-06 20:51:57 +0530

    some basic style for outlet

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,14 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import Navigation from '../components/navigation/navigation'
 
-import classes from './Root.module.scss';
+import './root.styles.scss';
 
 const RootLayout = () => {
 
     return (
         <>
         <Navigation />
-        <main className={classes.content}>
+        <main className='content'>
             <Outlet />
         </main>
         </>
