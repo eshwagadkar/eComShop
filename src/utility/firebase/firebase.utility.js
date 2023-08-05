8dd2b30678bafb37c75e885a9fd36cf637f2eca5
eshwagadkar <eshwagadkar007@gmail.com>
2023-08-05 19:00:21 +0530

    store the users in the firestore cloud database.

--- src/utility/firebase/firebase.utility.js
+++ src/utility/firebase/firebase.utility.js
@@ -7,6 +7,13 @@ import {
     GoogleAuthProvider,
 } from 'firebase/auth';
 
+import {
+  getFirestore,
+  doc, 
+  getDoc,
+  setDoc
+} from 'firebase/firestore';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -30,3 +37,30 @@ provider.setCustomParameters({
 
 export const auth = getAuth(); 
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+// initialize a firestore database instance
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapShot = await getDoc(userDocRef);
+  console.log(userSnapShot.exists());
+
+  // If user data doesnot exists 
+  if(!userSnapShot.exists()){
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    // Create and store in firestore cloud db.
+    try {
+      await setDoc(userDocRef, { displayName, email, createdAt });
+    } catch(err) {
+      console.log('Error creating the user', err.message );
+    }
+  }
+
+  // if exists return it
+  return userDocRef;
+  
+}
