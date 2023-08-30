cbecd8a2fcf18816d6f6bb6af1f07f4d0865d5b4
eshwagadkar <eshwagadkar007@gmail.com>
2023-08-30 19:46:54 +0530

    signup form component to create a new user

---
+++ src/components/signUpForm/signUpForm.js
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utility/firebase/firebase.utility';
+
+import FormInput from "../formInput/formInput";
+
+const defaultFormFields = {
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+}
+
+const SignUpForm = () => {
+
+    const [ formFields, setFormFields ] = useState(defaultFormFields);
+    const { displayName, email, password, confirmPassword } = formFields;
+
+    console.log(formFields)
+
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields);
+    }
+
+    const changeHandler = (event) => {
+       const {name, value} = event.target;
+
+       setFormFields({
+        ...formFields,
+        [name]: value
+       })
+    }
+
+    const submitHandler = async (event) => {
+        event.preventDefault();
+
+        // Password validation
+        if(password !== confirmPassword){
+            alert('Password do not match');
+            return;
+        }
+
+        try{
+            const { user } =  await createAuthUserWithEmailAndPassword(email, password)
+            await createUserDocumentFromAuth(user, { displayName })
+            resetFormFields();
+        }catch(error){
+            if(error.code ===  'auth/email-already-in-use'){
+                alert('Cannot create user, email already in use.')
+            } else {
+                console.log('Error', error);
+            }
+        }
+    }
+
+    return <>
+        <div>
+            <h1>Sign up using the email and password</h1>
+            <form onSubmit={submitHandler} >
+                {/* Display Name Input */}
+                <FormInput label='Display Name' type='text'  onChange={changeHandler} name='displayName' value={displayName} required />
+                {/* Email Input */}
+                <FormInput label='Email' type='email'  onChange={changeHandler} name='email' value={email} required />
+                {/* Password */}
+                <FormInput label='Password' type="password"  onChange={changeHandler} name='password' value={password} required />
+                {/* Confirm Password */}
+                <FormInput label='Confirm Password' type="password"  onChange={changeHandler} name='confirmPassword' value={confirmPassword} required />
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
+    </>
+}
+
+export default SignUpForm;
