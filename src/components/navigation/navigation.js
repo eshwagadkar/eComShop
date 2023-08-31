afe5f8d636338a39ac4f7f44b9fbb5136102a642
eshwagadkar <eshwagadkar007@gmail.com>
2023-08-31 18:36:25 +0530

    leverage context to signout

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,9 +1,20 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import { ReactComponent as Logo } from '../../assets/logo.svg';
+import { UserContext } from '../../contexts/userContext';
+import { signOutUser } from '../../utility/firebase/firebase.utility';
 
 import './navigation.styles.scss';
 const Navigation = () => {
+
+    const { currentUser, setCurrentUser } = useContext(UserContext);
+
+    const signOutHandler = async () => {
+        const res = await signOutUser();
+        setCurrentUser(null);
+    }
+
     return (
         <header>
             <nav className='navigation'>
@@ -15,7 +26,10 @@ const Navigation = () => {
                 {/* Navbar links */}
                 <ul className='nav-links-container'>
                     <li className='nav-link'><Link to="/about">About</Link></li>
-                    <li className='nav-link'><Link to="/authenticate">Authenticate</Link></li>
+                    { currentUser ? 
+                         <li className='nav-link' onClick={signOutHandler}>Sign Out</li> :
+                         <li className='nav-link'><Link to="/authenticate">Authenticate</Link></li> 
+                    }
                 </ul>
             </nav>
         </header>
