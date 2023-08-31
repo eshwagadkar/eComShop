dee77181c25a174e4cca0f291f536aed93217a8f
eshwagadkar <eshwagadkar007@gmail.com>
2023-08-31 18:04:44 +0530

    integrate user context

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 
+import {UserProvider} from './contexts/userContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <App /> 
+    </UserProvider>
   </React.StrictMode>
 );
