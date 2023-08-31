884fa208ef0f24a2bfb8acac7ef1b49b7569a938
eshwagadkar <eshwagadkar007@gmail.com>
2023-08-31 09:46:10 +0530

    logic to signin using email and pswd

--- src/utility/firebase/firebase.utility.js
+++ src/utility/firebase/firebase.utility.js
@@ -5,7 +5,8 @@ import {
     // signInWithRedirect,
     signInWithPopup,
     GoogleAuthProvider,
-    createUserWithEmailAndPassword
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
 } from 'firebase/auth';
 
 import {
@@ -82,4 +83,13 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
   return await createUserWithEmailAndPassword(auth, email, password);
 
 }
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+
+  if(!email || !password) return; 
+
+  return await signInWithEmailAndPassword(auth, email, password);
+
+}
+
 
